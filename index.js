d33f30b4ef1977f05fc8162aae16d4c1ad74c791
Mari-8 <ryanaltimari8@gmail.com>
2021-03-23 19:31:19 -0500

    commit tree 1cd7e2109cb14aaffd89c78d51af25953f19f257

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt().toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string) {
+    let stringSplit = string.split(" ")
+    let capString = []
+    let doNotInclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let i=0; i < stringSplit.length; i++) {
+      if (doNotInclude.includes(stringSplit[i])) {
+        let el = stringSplit[i]
+        capString.push(el)
+      } else {
+        let elem = this.capitalize(stringSplit[i])
+        capString.push(elem)
+      }
+    }
+    return this.capitalize(capString.join(" "))
+    
+  }
 }
+
